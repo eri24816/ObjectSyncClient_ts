20577fe63669887c22fbf332b2b8c0948a486499
eri24816 <a931eric@gmail.com>
2023-05-07 11:52:53 +0800

    Update

--- src/client.ts
+++ src/client.ts
@@ -4,31 +4,52 @@ import { Constructor } from "chatroom-client/src/utils"
 
 export class ObjectSyncClient{
     private readonly chatroom;
-    private readonly objects: Map<number,SObject> = new Map();
+    private readonly objects: Map<string,SObject> = new Map();
     private object_ids: SetTopic|null = null;
     private createObjectEvent: EventTopic|null = null;
     constructor(host: string){
         this.chatroom = new ChatroomClient(host);
         this.chatroom.onConnected(() => {
-            this.object_ids = this.chatroom.getTopic('object_ids',SetTopic);
-            this.object_ids.onAppend.add(this.onObjectIdsAppend.bind(this));
-            this.object_ids.onRemove.add(this.onObjectIdsRemove.bind(this));
-            this.createObjectEvent = this.chatroom.getTopic('create_object',EventTopic);
+            this.defineTransitions();
         });
+    }
+
+    private defineTransitions(): void{
+        // callbacks go brrrr
+
+        this.object_ids = this.chatroom.getTopic('object_ids',SetTopic);
+        this.object_ids.onAppend.add(
+            (id: string) => {
+                const obj = new SObject(this, id);
+                this.objects.set(id,obj);
+            }
+        );
+        this.object_ids.onRemove.add(
+            (id: string) => {
+                const obj = this.objects.get(id)!;
+                obj.onDestroy();
+                this.objects.delete(id);
+            }
+        );
+
+        // this.createObjectEvent = this.chatroom.getTopic('create_object',EventTopic);
+        // this.createObjectEvent.onEmit.add(
+        //     (data: {parent_id: number}) => {
+        //         const id = this.object_ids!.append();
+        //         this.objects.get(id)!.parent_id.set(data.parent_id);
+        //     }
+        // );
+        this.chatroom.on('create_object', 
+            // unpack data
+            ({id,parent_id}:{'id':string,'parent_id':string}) => {
+                // Simulate what the server would do
+
+            }
+        );
     }
     
     public createObject(parent:SObject): void{
         this.createObjectEvent!.emit({parent_id:parent.id});
-    }
-
-    private onObjectIdsAppend(id: number): void{
-        const obj = new SObject(this, id);
-        this.objects.set(id,obj);
-    }
-    private onObjectIdsRemove(id: number): void{
-        const obj = this.objects.get(id);
-        obj!.onDestroy();
-        this.objects.delete(id);
     }
 
     /* Encapsulate ChatRoom */
--- src/sobject.ts
+++ src/sobject.ts
@@ -3,20 +3,16 @@ import { ObjectSyncClient } from "./client"
 
 export class SObject{
     private readonly _id
-    public get id(): number{
+    public get id(): string{
         return this._id;
     }
     private readonly objectsync;
     private readonly parent_id;
-    constructor(objectsync: ObjectSyncClient, id: number){
+    constructor(objectsync: ObjectSyncClient, id: string){
         this.objectsync = objectsync;
         this._id = id;
         this.parent_id = objectsync.getTopic(`_/${id}/parent_id`,IntTopic);
         this.parent_id.onSet2.add(this.onParentChanged.bind(this));
-    }
-
-    getId(): number{
-        return this.id;
     }
 
     onParentChanged(oldValue: number, newValue: number): void{
