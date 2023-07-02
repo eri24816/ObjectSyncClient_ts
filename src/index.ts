8b92a522fc6931cc643729cd6b18d927b8385657
eri24816 <a931eric@gmail.com>
2023-07-02 21:04:50 +0800

    Remove BinaryTopic from exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export {ObjectSyncClient} from './client';
 export {SObject} from './sobject';
-export { Topic, StringTopic, DictTopic, IntTopic, SetTopic, FloatTopic, GenericTopic, ListTopic, EventTopic, BinaryTopic } from 'chatroom-client/src';
+export { Topic, StringTopic, DictTopic, IntTopic, SetTopic, FloatTopic, GenericTopic, ListTopic, EventTopic } from 'chatroom-client/src';
 export {ObjectTopic, ObjSetTopic, ObjListTopic, ObjDictTopic} from './topic';
 export {Action} from 'chatroom-client/src/utils';
